eca259246e548f23ad9b326ffe8c7fc3120837d3
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-10-30 13:26:01 +0530

    edit

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Author = require('../models/author ')
+const Author = require('../models/author.js')
 
 
 // all authors route
